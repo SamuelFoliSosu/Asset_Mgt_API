ef6e6238bbc8837713395c1c98d9538c5462c57a
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-09-04 23:36:32 +0000

    mapping user who made change to changed_by_user field automatically

--- asset_management_api/asset_status_history/serializers.py
+++ asset_management_api/asset_status_history/serializers.py
@@ -4,9 +4,19 @@ from .models import AssetStatusHistory
 class AssetStatusHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = AssetStatusHistory
-        fields = "__all__"
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = [
+            "id",
+            "asset",
+            "status",
+            "changed_by_user",   #read-only
+            "change_date",
+            "comment",
+        ]
+        read_only_fields = ["id", "changed_by_user", "change_date"]
 
     def create(self, validated_data):
-        # Ensure history is append-only (no updates to past records)
+        # Inject the current request user automatically
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["changed_by_user"] = request.user
         return super().create(validated_data)
