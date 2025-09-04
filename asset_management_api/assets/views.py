af84ef0e479ac84700124f589819fc928bac18c6
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-09-04 22:31:00 +0000

    Set up filters and search for Assets

--- asset_management_api/assets/views.py
+++ asset_management_api/assets/views.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Asset
 from .serializers import AssetSerializer
 
 class AssetViewSet(viewsets.ModelViewSet):
     queryset = Asset.objects.all()
     serializer_class = AssetSerializer
+
+    # Enable filtering + search
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+
+    # Filtering by FK relations
+    filterset_fields = ['location', 'owner_id', 'location__department_id']
+
+    # Search across text fields
+    search_fields = ['asset_name', 'serial_number', 'model']
