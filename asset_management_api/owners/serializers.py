d7fae9eb9b7da40f955ac671fe9dd1cc3f6fae67
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-09-08 21:07:30 +0000

    Testing code after beefups and error fixes

--- asset_management_api/owners/serializers.py
+++ asset_management_api/owners/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from .models import Owner
 
 class OwnerSerializer(serializers.ModelSerializer):
+    location_name = serializers.CharField(source='location.name', read_only=True)
+    department_name = serializers.CharField(source='location.department.name', read_only=True)
+
     class Meta:
         model = Owner
         fields = [
-            'id', 'department', 'location', 'name', 'owner_type',
+            'id', 'location', 'location_name', 'department_name', 'name', 'owner_type',
             'contact_email', 'phone', 'created_at', 'updated_at'
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        return Owner.objects.create(**validated_data)
